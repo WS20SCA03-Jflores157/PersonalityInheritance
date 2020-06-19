7967ebecd0bb994afa41b78b1471489a2f9d8240
Jeffrey <Jeffrey@simpleyapp.com>
2020-06-19 18:51:12 -0400

    Initial Commit

---
+++ PersonalityInheritance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityInheritance
+//
+//  Created by Jeffrey  on 6/19/20.
+//  Copyright © 2020 BMCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
